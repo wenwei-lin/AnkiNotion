35a1dedd90104fb3c92565bfdc75bfc797b60466
Wenwei Lin <wenwei.lin.me@gmail.com>
2022-08-28 21:42:36 +0800

    Build Azure audio class

--- audio.py
+++ audio.py
@@ -1,36 +1,14 @@
-# Copyright (c) Microsoft. All rights reserved.
-# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
-
-# <code>
 import azure.cognitiveservices.speech as speechsdk
 
-# Creates an instance of a speech config with specified subscription key and service region.
-# Replace with your own subscription key and service region (e.g., "westus").
-speech_key, service_region = "YourSubscriptionKey", "YourServiceRegion"
-speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=service_region)
+class AzureAudio:
+    def __init__(self, key, region) -> None:
+        self.key = key
+        self.region = region
+        self.speech_config = speechsdk.SpeechConfig(subscription=self.key, region=self.region)
+        self.speech_config.speech_synthesis_language = "en-US" 
+        self.speech_config.speech_synthesis_voice_name = "en-US-ChristopherNeural"
+        self.audio_config = speechsdk.audio.AudioOutputConfig(filename="temp.wav")
+        self.synthesizer = speechsdk.SpeechSynthesizer(speech_config=self.speech_config, audio_config=self.audio_config)
 
-# Set the voice name, refer to https://aka.ms/speech/voices/neural for full list.
-speech_config.speech_synthesis_voice_name = "en-US-AriaNeural"
-
-# Creates a speech synthesizer using the default speaker as audio output.
-speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config)
-
-# Receives a text from console input.
-print("Type some text that you want to speak...")
-text = input()
-
-# Synthesizes the received text to speech.
-# The synthesized speech is expected to be heard on the speaker with this line executed.
-result = speech_synthesizer.speak_text_async(text).get()
-
-# Checks result.
-if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-    print("Speech synthesized to speaker for text [{}]".format(text))
-elif result.reason == speechsdk.ResultReason.Canceled:
-    cancellation_details = result.cancellation_details
-    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
-    if cancellation_details.reason == speechsdk.CancellationReason.Error:
-        if cancellation_details.error_details:
-            print("Error details: {}".format(cancellation_details.error_details))
-    print("Did you update the subscription info?")
-# </code>
+    def synthesize_audio(text):
+        self.synthesizer.speak_text_async(text)
