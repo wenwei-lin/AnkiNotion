58efae4a5d711ea70e24e2d4a560aff90a3bbb80
Wenwei Lin <wenwei.lin.me@gmail.com>
2022-09-04 17:34:35 +0800

    Rename module

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
 from notion.client import NotionClient
+from anki.client import AnkiClient
+from dictionary.merria_webster import MerriaWebster
+from cloud_service.speech import AzureSpeech
 import os
 import re
 import base64
@@ -7,8 +10,14 @@ import base64
 
 def main():
     load_dotenv()
-    notion = NotionClient(token_v2=os.getenv("NOTION_TOKEN"))
+    with open('/etc/resolv.conf', 'r') as wsl_file:
+        wsl_ip = wsl_file.readlines()[-1].split()[1]
     
+    notion = NotionClient(token_v2=os.getenv("NOTION_TOKEN"))
+    anki = AnkiClient(f"http://{wsl_ip}:8765")
+    merria_webster = MerriaWebster(os.getenv("MERRIA_WEBSTER_TOKEN"))
+    azure_speech = AzureSpeech(key=os.getenv("AZURE_SPEECH_KEY"), region=os.getenv("AZURE_SPEECH_REGION"))
+
     
     # Fetch not added records from Notion Database
     database_id = os.getenv("NOTION_DATABASE_ID")
